d474ad2eef4e928cb6db207f86b136c6d88d18fb
Kennedy Tan <ktan0087@student.monash.edu>
2023-10-06 15:49:15 +0800

    create W10 folder

---
+++ W10/vega_lite_vis.js
@@ -0,0 +1,13 @@
+var vg_1 = "air_pollution_map.vg.json";
+vegaEmbed("#map_chart", vg_1)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_1 = "air_pollution_bubble.vg.json";
+vegaEmbed("#bubble_chart", vg_1)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);

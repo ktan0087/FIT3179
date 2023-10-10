232b9cc1e91cd6fff3729b7725369ee92d558650
Kennedy Tan <ktan0087@student.monash.edu>
2023-10-11 05:25:31 +0800

    update bar chart

--- DV2/vega_lite_vis.js
+++ DV2/vega_lite_vis.js
@@ -18,3 +18,10 @@ vegaEmbed("#scatter_chart", vg_3)
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
   .catch(console.error);
+
+var vg_4 = "air_pollution_bar.vg.json";
+vegaEmbed("#bar_chart", vg_4)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);

e81222d5654c0a8e4c58ae244120683fa7e3217f
Kennedy Tan <ktan0087@student.monash.edu>
2023-10-11 03:29:44 +0800

    create files for idioms

---
+++ DV2/vega_lite_vis.js
@@ -0,0 +1,20 @@
+var vg_1 = "air_pollution_map.vg.json";
+vegaEmbed("#map_chart", vg_1)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_2 = "air_pollution_bubble.vg.json";
+vegaEmbed("#bubble_chart", vg_2)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+var vg_3 = "air_pollution_scatter.vg.json";
+vegaEmbed("#scatter_chart", vg_3)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);

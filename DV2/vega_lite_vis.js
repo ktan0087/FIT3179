5bb10ebeef90558c3b72b94e3e8df01cf6f99b72
Kennedy Tan <ktan0087@student.monash.edu>
2023-10-16 01:25:49 +0800

    completed scatter plot

--- DV2/vega_lite_vis.js
+++ DV2/vega_lite_vis.js
@@ -5,13 +5,6 @@ vegaEmbed("#map_chart", vg_1)
   })
   .catch(console.error);
 
-var vg_2 = "air_pollution_bubble.vg.json";
-vegaEmbed("#bubble_chart", vg_2)
-  .then(function (result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  })
-  .catch(console.error);
-
 var vg_3 = "air_pollution_scatter.vg.json";
 vegaEmbed("#scatter_chart", vg_3)
   .then(function (result) {
